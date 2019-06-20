8d1dd8b4bb6a3da39c3227c82d5a1915d2127819
jefffang19 <jefffang07@gmail.com>
2019-06-20 10:37:56 +0800

    step 6

---
+++ step_6/client.cpp
@@ -0,0 +1,107 @@
+#include "client.h"
+
+vector<unsigned char> out;
+int numbth_client;  //you can be 1~4
+int num_of_files;
+int request_file[5];
+
+
+void wfile(int num);
+
+int main(){
+	srand(time(NULL));
+	
+	Client myclient;
+	
+	cout << "input # client : 1\n";
+	numbth_client = 1;
+	cout << "request how many files : 1\n";
+	num_of_files = 1;
+	cout << "request video file number : 3\n";
+	for(int i = 0;i < num_of_files;++i) request_file[i] = 3;
+	
+	//set src ip and port
+	myclient.child.myCreateSocket(client_ip,client_port[numbth_client-1]);
+	myclient.initInfo();
+	
+	//init connection
+	myclient.child.myConnect(server_ip,server_port);
+	
+	myclient.child.inithandshake(numbth_client, num_of_files, request_file);
+	
+	
+	//get all the files request
+	for(int i=0;i<num_of_files;++i){
+		myclient.recvfile();
+		wfile(i+1);
+		out.clear();
+	}
+	
+	return 0;
+}
+
+void wfile(int num){
+	stringstream ss;
+	ss << numbth_client;
+	string n = ss.str();
+	stringstream ss2;
+	ss2 << num;
+	string numf = ss2.str();
+	ofstream outf( "client" + n + "-out" + numf + ".mp4", ios::binary);
+	
+	char temp[out.size()];
+	for(int i=0;i<out.size();++i){
+		temp[i] = out[i];
+	}
+	copy(temp,temp+out.size(),ostreambuf_iterator<char>(outf));
+	outf.close();
+	
+	cout << "client" << n << "-out" << numf << ".mp4 created , size :" << out.size() << " bytes\n";
+}
+
+void Client::initInfo(){
+	cout << "====================" << endl <<
+			"RTT delay = "	<< child.RTT << "ms" << endl <<
+			"MSS = " << child.MSS << " bytes" << endl <<
+			"buffer size = "<< default_BUFFER_SIZE << " bytes" << endl <<
+			"loss = "<< child.loss << " %" << endl <<
+			"Client IP & port= " << child.addr(child.srcSocket) << endl <<
+			"====================" << endl;
+}
+void Client::recvfile(){
+	bool isEnd = false;
+	int timeout;
+	Packet recv_packet;
+	
+	while(!isEnd){
+		cout << "Listening\n";
+		
+		recv_packet = child.myRecv(&timeout,false);
+		
+		//if timeout happened, send dup ack
+		if(timeout==-1){
+			child.mySend(Packet(packet_ack,this->child,(char*)NULL));
+			continue;
+		}
+		
+		//cout << "Receive file from " << child.addr(child.destSocket) << endl;
+		if(recv_packet.packet_type() == packet_data) {
+				// if new ack then recv, else ignore
+				if(child.isNewAck(recv_packet) && child.updateNum(recv_packet)){
+					for(int i = 0;i<child.MSS;++i) fileBuffer[i] = recv_packet.data[i];
+					for(int i = 0;i<child.MSS;++i) out.push_back(fileBuffer[i]);
+				}
+				child.mySend(Packet(packet_ack,this->child,(char*)NULL));
+		}
+		//when the ack packet of threeway handshake loss
+		else if(recv_packet.packet_type() == packet_synack){
+			cout << "Server did not complete threeway handshake\ntry to fix\n";
+			child.mySend(Packet(packet_ack,this->child,(char*)NULL));
+		}
+		else if(recv_packet.packet_type() == packet_fin){
+			cout << "Receive file complete\n";
+			isEnd = true;
+		}
+	}
+	cout << "got file len = " << out.size() << "bytes" << endl;
+}
---
+++ step_6/server.h
@@ -0,0 +1,28 @@
+#include "tcpconnect.h"
+
+class Server_process : public Tcpconnect {
+	public:
+		pid_t childpid[num_of_clients];
+		int childcnt;
+		vector<int> req_files;
+		
+		bool masterchild;
+		void inithandshake();
+		void endhandshake();
+		Server_process(){
+			childcnt = 0;
+			masterchild = false;
+		}
+};
+
+class Server {
+	public:
+		Server_process child;
+		char fileBuffer[default_BUFFER_SIZE];
+
+		Server();
+		void initInfo();
+		void printStatus();
+		int sendfile(const char *data, const int dataSize = 0);  //return how many segments
+};
+
